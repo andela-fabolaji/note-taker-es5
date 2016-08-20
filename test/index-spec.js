4a7e1b13581f89cdbf523f52cdac01e9068b1a34
femipixels <femi.systems@gmail.com>
2016-08-20 06:38:19 +0100

    Round up

--- test/index-spec.js
+++ test/index-spec.js
@@ -5,21 +5,22 @@ var expect = chai.expect;
 
 describe('Note Taker Class', function(){
 
-    //create globals
-    var anonymous = new NoteTaker();
-    var femi = new NoteTaker('Abolaji Femi');
-
-    //inject test data
-    femi.notes.push('Andela\'s bootcamp is dope');
-    femi.notes.push('Hello from the other side');
-
     describe('Testing for instantiation of class', function(){
         it('New Object should exist', function(){
+            var anonymous = new NoteTaker();
+            var femi = new NoteTaker('Abolaji Femi');
+
             assert.instanceOf(anonymous, NoteTaker);
             assert.instanceOf(femi, NoteTaker);
         });
 
         it('New Object Anonymous or Femi should have base properties -> author, notes', function(){
+            var anonymous = new NoteTaker();
+            var femi = new NoteTaker('Abolaji Femi');
+    
+            femi.createNote('Andela\'s bootcamp is dope');
+            femi.createNote('Hello from the other side');
+            
             assert(anonymous.hasOwnProperty('author'));
             assert(anonymous.hasOwnProperty('notes'));
             assert(femi.hasOwnProperty('author'));
@@ -29,134 +30,167 @@ describe('Note Taker Class', function(){
 
     describe('isEmptyNotes method', function(){
 
-        it('Should return true if there are no notes in DB', function(){
-            expect(femi.isEmptyNotes()).to.equal(false);
+        it('Should return true if no notes have been created', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+
+            assert.isTrue(femi.isEmptyNotes());
         });
 
     });
 
     describe('isValidNoteId method', function(){
-        var note_id = parseInt('1');
 
-        it('Note id should be an integer value', function(){
-            assert.typeOf(note_id, 'number');
+        it('Should return true if note_id is valid', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('A simple note');
+
+            assert.isTrue(femi.isValidNoteId(0));
         });
 
-        it('Note id should not be greater than length of femi.notes', function(){
-            assert(note_id < femi.notes.length);
-        });
-
-        it('Note id should not be less than 0', function(){
-            assert(note_id >= 0);
-        });
-
-        it('Note id should not be less than 1 digit', function(){
-            assert(note_id.toString.length > 0);
-        });
-
-        it('With other conditions true, note id should be valid', function(){
-            assert(femi.isValidNoteId(note_id), true);
-        });
-    });
-
-    describe('ListNotes method', function(){ 
-        it('Should return a string msg', function(){
-            expect(femi.listNotes()).to.equal('0\nAndela\'s bootcamp is dope\n\n1\nHello from the other side\n\nAbolaji Femi');
-        });
     });
 
     describe('CreateNote method', function(){ 
-        it('Should successfully save note if a parameter is passed', function(){
+        it('Should successfully save note if a argument is passed', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+
             assert(femi.createNote('Some message'), 'Note successfully created');
         });
 
-        it('Should return failure msg if no parameter is passed', function(){
-            assert(femi.createNote(), 'Cannot create an empty note!');
+        it('Should return failure msg if no argument is passed', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+
+            expect(femi.createNote()).to.equal('Cannot create an empty note!');
         });
     });    
 
+    describe('ListNotes method', function(){ 
+        it('Should return a string msg if there are no notes', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote();
+
+            expect(femi.listNotes()).to.equal('There are no notes in the database');
+        });
+
+        it('Should return an array of notes if there are notes', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('A simple note');
+
+            assert.isTrue(Array.isArray(femi.listNotes()));
+        });
+    });
+
     describe('GetNote method', function(){
-        var note_id = 5;
 
-        it('Should return error msg if no parameter is passed in', function(){
-            expect(femi.getNote()).to.equal('Please specify an ID');
+        it('Should return error if no argument is passed or db empty', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+
+            assert(femi.getNote(), 'Invalid ID');
         });
 
-        it('Should return accept an integer', function(){
-            assert.typeOf(note_id, 'number');
+        it('Should return error if wrong argument is passed', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+
+            assert(femi.getNote('id'), 'Invalid ID');
         });
 
-        it('Should return a string msg', function(){
-            expect(femi.getNote(note_id)).to.equal('Invalid ID');
-        });
-    });
-
-    describe('DeleteNote method', function(){
-        var note_id = 5;
-
-        it('Should return error msg if no parameter is passed in', function(){
-            expect(femi.deleteNote()).to.equal('Please specify an ID');
+        it('Should accept an integer id and return note string', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('A simple note item');
+            
+            assert(femi.getNote(0), 'A simple note item');
         });
 
-        it('Should return accept an integer', function(){
-            assert.typeOf(note_id, 'number');
+        it('Should accept an string number id and return note string', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('A simple note item');
+            
+            assert(femi.getNote('0'), 'A simple note item');
         });
-
-        it('Should return a string msg depending on success-failure', function(){
-            expect(femi.deleteNote(note_id)).to.equal('Invalid ID');
-            expect(femi.deleteNote(1)).to.equal('Note successfully deleted');
-        });
-    });
-
-    describe('EditNote Method', function(){
-
-        var note_id = 1;
-        var new_content = 'Some very new content';
-
-        it('Should not accept null parameters', function(){
-            assert(femi.editNote(), 'Invalid Input');
-        });
-
-        it('Should not accept single interger parameter', function(){
-            assert(femi.editNote(5), 'Invalid Input');
-        });
-
-        it('Should not accept single string parater', function(){
-            assert(femi.editNote('string'), 'Invalid Input');
-        });
-
-        it('Should accept both parameters as string and test for explicit conversion', function(){
-            assert(femi.editNote('0', '5 or string'), 'Note successfully updated');
-        });        
-
-        it('Should not accept parameter 1 as string', function(){
-            assert(femi.editNote('string', 5), 'Invalid input');
-        });
-
-        it('Should accept both parameters as integers', function(){
-            assert(femi.editNote(5, 5), 'Note successfully updated');
-        });
-
-        it('Should return a success message', function(){
-            expect(femi.editNote(note_id, new_content)).to.equal('Note successfully updated');
-        });        
 
     });
 
     describe('Search Method', function(){
 
-        var tunde = new NoteTaker();
-        tunde.notes.push('Some cool stuff');
-
-        it('Should not take a null search parameter', function(){
+        it('Should not take a null search argument', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Andela is super cool');
+            
             assert(femi.search(), 'Cannot search for empty string');
         });
 
         it('Should return an array of search results', function(){
-            assert.isTrue(Array.isArray(femi.search('cool')));
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Become world class');
+            
+            assert.isTrue(Array.isArray(femi.search('class')));
         });
 
     });
 
+    describe('DeleteNote method', function(){
+
+        it('Should return error if null/wrong argument is passed or if db is empty', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+
+            assert(femi.deleteNote(), 'Invalid ID');
+            assert(femi.deleteNote(0), 'Invalid ID');
+            assert(femi.deleteNote('id'), 'Invalid ID');
+        });
+
+        it('Should return a success message if action is successful', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Doing stuffs with javascript is cool');
+
+            assert(femi.deleteNote(0), 'Note successfully deleted');
+            assert(femi.deleteNote('0'), 'Note successfully deleted');
+        });
+
+    });
+
+    describe('EditNote Method', function(){
+
+        it('Should not accept null arguments', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Some really serious testing');
+
+            assert(femi.editNote(), 'Invalid Input');
+        });
+
+        it('Should not accept single interger argument', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Some really serious testing');
+
+            assert(femi.editNote(5), 'Invalid Input');
+        });
+
+        it('Should not accept single string parater', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Some really serious testing');
+
+            assert(femi.editNote('string'), 'Invalid Input');
+        });
+
+        it('Should not accept first argument as string', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Some really serious testing');
+
+            assert(femi.editNote('string', 5), 'Invalid input');
+        });
+
+        it('Should accept both arguments as string and test for explicit conversion', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Some really serious testing');
+
+            assert(femi.editNote('0', '5 or string'), 'Note successfully updated');
+        });        
+
+        it('Should accept both arguments as integers', function(){
+            var femi = new NoteTaker('Abolaji Femi');
+            femi.createNote('Some really serious testing');
+
+            assert(femi.editNote(5, 5), 'Note successfully updated');
+        });
+
+    });
 
 });
